3e43de1c56113c23c1d1ea7588f39b3599ef2c65
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-08-18 11:15:27 +0200

    Don't use NULL

--- src/string_bidi.cpp
+++ src/string_bidi.cpp
@@ -4,7 +4,7 @@ std::vector<int> get_bidi_embeddings(const uint32_t* string, int n_chars) {
   FriBidiCharType base_direction = FRIBIDI_TYPE_ON;
   std::vector<FriBidiLevel> embedding_levels(n_chars);
 
-  fribidi_log2vis(string, n_chars, &base_direction, NULL, NULL, NULL, embedding_levels.data());
+  fribidi_log2vis(string, n_chars, &base_direction, nullptr, nullptr, nullptr, embedding_levels.data());
 
   return {embedding_levels.begin(), embedding_levels.end()};
 }
