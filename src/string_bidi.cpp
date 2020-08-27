27e76b01987b7aaee3b6b8410aba3735e1c17d8d
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-08-27 12:11:39 +0200

    Fix #2

--- src/string_bidi.cpp
+++ src/string_bidi.cpp
@@ -4,7 +4,10 @@ std::vector<int> get_bidi_embeddings(const uint32_t* string, int n_chars) {
   FriBidiCharType base_direction = FRIBIDI_TYPE_ON;
   std::vector<FriBidiLevel> embedding_levels(n_chars);
 
-  fribidi_log2vis(string, n_chars, &base_direction, nullptr, nullptr, nullptr, embedding_levels.data());
+  FriBidiLevel max_level = fribidi_log2vis(string, n_chars, &base_direction,
+                                           nullptr, nullptr, nullptr,
+                                           embedding_levels.data());
+  (void) max_level; //shut up compiler about unused variables
 
   return {embedding_levels.begin(), embedding_levels.end()};
 }
