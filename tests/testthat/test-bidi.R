8accc37adb60f719e872e379cf7390ae5a79fa95
Thomas Lin Pedersen <thomasp85@gmail.com>
2025-01-13 16:32:20 +0100

    move bidi testing to testthat

--- tests/testthat/test-bidi.R
+++ tests/testthat/test-bidi.R
@@ -1,26 +1,22 @@
-run_bidi_test <- function(fail = TRUE) {
+test_that("Textshaping follows UNICODE Bidi conventions", {
+  skip_on_cran()
+  skip_on_ci()
+
+  # Change to FALSE to run tests locally
+  skip_if(TRUE, "Skipping time consuming UNICODE Bidi tests")
+
   test_url <- "https://www.unicode.org/Public/UCD/latest/ucd/BidiCharacterTest.txt"
   tests <- read.csv2(test_url, header = FALSE, comment.char = "#")
   names(tests) <- c("unicode", "ltr_set", "ltr_detected", "bidi_embedding", "visual_order")
-  tests$bidi_embedding <- lapply(strsplit(tests$bidi_embedding, " ", TRUE), as.integer)
   tests$visual_order <- lapply(strsplit(tests$visual_order, " ", TRUE), as.integer)
-  message("Running ", nrow(tests), " tests")
-  pb <- utils::txtProgressBar(max = nrow(tests), style = 3)
-  on.exit(close(pb))
-  fails <- integer()
   for (i in seq_len(nrow(tests))) {
-    utils::setTxtProgressBar(pb, i)
     test_string <- paste0("\"\\u", gsub(" ", "\\u", tests$unicode[i], fixed = TRUE), "\"")
     test_string <- eval(parse(text = test_string))
     dir <- c("ltr", "rtl", "auto")[tests$ltr_set[i] + 1]
     shape <- shape_text(test_string, direction = dir)
-    if (shape$metrics$ltr != (tests$ltr_detected[i] == 0)) {
-      if (fail) {
-        stop("Test ", i, " failed! Wrong paragraph level embedding detected")
-      }
-      fails <- c(fails, i)
-      next
-    }
+
+    expect_equal(shape$metrics$ltr, tests$ltr_detected[i] == 0, label = paste0("calculated direction [test ", i,"]"))
+
     true_ordering <- tests$visual_order[[i]] + 1L
 
     # We do the below to make sure that any ligature substitutions doesn't affect the
@@ -28,13 +24,6 @@ run_bidi_test <- function(fail = TRUE) {
     true_ordering <- intersect(true_ordering, shape$shape$glyph)
     calc_ordering <- intersect(shape$shape$glyph, true_ordering)
 
-    if (!identical(true_ordering, calc_ordering)) {
-      if (fail) {
-        stop("Test ", i, " failed! Wrong visual order")
-      }
-      fails <- c(fails, i)
-      next
-    }
+    expect_equal(calc_ordering, true_ordering, label = paste0("calculated visual order [test ", i,"]"))
   }
-  invisible(fails)
-}
+})
