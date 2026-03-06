7537431226d1bf76c4188f5c9d24e6a6305c4119
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-03-06 10:15:02 +0100

    move to .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(...) {
+.onAttach <- function(...) {
   if (!get_systemfont_cache_compat()) {
     packageStartupMessage(
       "systemfonts and textshaping have been compiled with different versions of Freetype.\nBecause of this, textshaping will not use the global font cache provided by systemfonts"
